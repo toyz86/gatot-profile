fc9ff7d6dedee5a7085e4d9b2f2c2dd2e6890f04
Gatotz <gatotprastyo@mmk-studio.co.id>
2023-01-18 22:11:24 +0700

    update node-sass

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,13 +1,36 @@
-import { wrapFunctional } from './utils'
-
 export { default as Footer } from '../../components/footer.vue'
 export { default as Gallery } from '../../components/gallery.vue'
 export { default as Header } from '../../components/header.vue'
 export { default as Loading } from '../../components/loading.vue'
 export { default as RelatedItems } from '../../components/related-items.vue'
 
-export const LazyFooter = import('../../components/footer.vue' /* webpackChunkName: "components/footer" */).then(c => wrapFunctional(c.default || c))
-export const LazyGallery = import('../../components/gallery.vue' /* webpackChunkName: "components/gallery" */).then(c => wrapFunctional(c.default || c))
-export const LazyHeader = import('../../components/header.vue' /* webpackChunkName: "components/header" */).then(c => wrapFunctional(c.default || c))
-export const LazyLoading = import('../../components/loading.vue' /* webpackChunkName: "components/loading" */).then(c => wrapFunctional(c.default || c))
-export const LazyRelatedItems = import('../../components/related-items.vue' /* webpackChunkName: "components/related-items" */).then(c => wrapFunctional(c.default || c))
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
--- .nuxt/components/plugin.js
+++ .nuxt/components/plugin.js
@@ -1,13 +1,5 @@
 import Vue from 'vue'
-import { wrapFunctional } from './utils'
-
-const components = {
-  Footer: () => import('../../components/footer.vue' /* webpackChunkName: "components/footer" */).then(c => wrapFunctional(c.default || c)),
-  Gallery: () => import('../../components/gallery.vue' /* webpackChunkName: "components/gallery" */).then(c => wrapFunctional(c.default || c)),
-  Header: () => import('../../components/header.vue' /* webpackChunkName: "components/header" */).then(c => wrapFunctional(c.default || c)),
-  Loading: () => import('../../components/loading.vue' /* webpackChunkName: "components/loading" */).then(c => wrapFunctional(c.default || c)),
-  RelatedItems: () => import('../../components/related-items.vue' /* webpackChunkName: "components/related-items" */).then(c => wrapFunctional(c.default || c))
-}
+import * as components from './index'
 
 for (const name in components) {
   Vue.component(name, components[name])
