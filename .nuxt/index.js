d56923b910f7e472edb4d9d11524ee9869dfec69
Gatotz <gatotprastyo@mmk-studio.co.id>
2023-02-06 17:14:23 +0700

    no message

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -45,7 +45,7 @@ Vue.component(Nuxt.name, Nuxt)
 
 Object.defineProperty(Vue.prototype, '$nuxt', {
   get() {
-    const globalNuxt = this.$root.$options.$nuxt
+    const globalNuxt = this.$root ? this.$root.$options.$nuxt : null
     if (process.client && !globalNuxt && typeof window !== 'undefined') {
       return window.$nuxt
     }
@@ -70,9 +70,9 @@ function registerModule (path, rawModule, options = {}) {
 }
 
 async function createApp(ssrContext, config = {}) {
-  const router = await createRouter(ssrContext, config)
-
   const store = createStore(ssrContext)
+  const router = await createRouter(ssrContext, config, { store })
+
   // Add this.$router into store actions/mutations
   store.$router = router
 
@@ -154,6 +154,7 @@ async function createApp(ssrContext, config = {}) {
     req: ssrContext ? ssrContext.req : undefined,
     res: ssrContext ? ssrContext.res : undefined,
     beforeRenderFns: ssrContext ? ssrContext.beforeRenderFns : undefined,
+    beforeSerializeFns: ssrContext ? ssrContext.beforeSerializeFns : undefined,
     ssrContext
   })
 
--- components/header.vue
+++ components/header.vue
@@ -1,29 +1,31 @@
 <template>
   <section class="menu-head">
     <header class="wrap d-flex align-items-center" id="header">
-      <a href="/" class="name-title">Gatot Prastyo</a>
-      <div class="header-socmed">
-        <a href="https://web.facebook.com/toyzefbean/" target="_blank"><i class="fa fa-facebook"></i></a>
-        <a href="https://api.whatsapp.com/send?phone=628567916365" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
-        <a href="https://www.linkedin.com/in/gatot-prastyo-56572641/" target="_blank"><i class="fa fa-linkedin"></i></a>
-        <a href="https://github.com/toyz86" target="_blank"><i class="fa fa-github"></i></a>    
+      <nuxt-link to="/" class="name-title mr-auto p-2">Gatot Prastyo</nuxt-link>
+      <div class="header-socmed p-2 d-none d-lg-block">
+        <nuxt-link to="https://web.facebook.com/toyzefbean/" target="_blank"><i class="fa fa-facebook"></i></nuxt-link>
+        <nuxt-link to="https://api.whatsapp.com/send?phone=628567916365" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></nuxt-link>
+        <nuxt-link to="https://www.linkedin.com/in/gatot-prastyo-56572641/" target="_blank"><i class="fa fa-linkedin"></i></nuxt-link>
+        <nuxt-link to="https://github.com/toyz86" target="_blank"><i class="fa fa-github"></i></nuxt-link>    
       </div>
 
-      <input id="hamburger" class="hamburger" type="checkbox"/>
-      <label class="hamburger" for="hamburger">
-        <i></i>
-      </label>
+      <div class="burger_nav p-2">
+        <input id="hamburger" class="hamburger" type="checkbox"/>
+        <label class="hamburger" for="hamburger">
+          <i></i>
+        </label>
+      </div>
 
       <div class="drawer-list">
         <ul @click="hideNav()">
           <li><nuxt-link to="/">Home</nuxt-link></li>
-          <li><nuxt-link to="/#gallery">Works</nuxt-link></li>
+          <!-- <li><nuxt-link to="/#gallery">Works</nuxt-link></li> -->
           <li><nuxt-link to="/about">About</nuxt-link></li>
-          <div v-if="isMobileSize()" class="socmed-nav d-flex justify-content-center">
-            <li><a href="https://web.facebook.com/toyzefbean/" target="_blank"><i class="fa fa-facebook"></i></a></li>
-            <li><a href="https://api.whatsapp.com/send?phone=628567916365" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/gatot-prastyo-56572641/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://github.com/toyz86" target="_blank"><i class="fa fa-github"></i></a></li>
+          <div class="socmed-nav d-flex d-block d-lg-none justify-content-center">
+            <li><nuxt-link to="https://web.facebook.com/toyzefbean/" target="_blank"><i class="fa fa-facebook"></i></nuxt-link></li>
+            <li><nuxt-link to="https://api.whatsapp.com/send?phone=628567916365" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></nuxt-link></li>
+            <li><nuxt-link to="https://www.linkedin.com/in/gatot-prastyo-56572641/" target="_blank"><i class="fa fa-linkedin"></i></nuxt-link></li>
+            <li><nuxt-link to="https://github.com/toyz86" target="_blank"><i class="fa fa-github"></i></nuxt-link></li>
           </div>          
         </ul>
       </div>    
@@ -48,22 +50,21 @@ export default {
     hideNav() {
       document.getElementById('hamburger').checked = false;
     },
-    isMobileSize() {
-      const maxSize = 480;
-      if(this.windowWidth > maxSize){
-        return false;
-      } else {
-        return true;
-      }
-    },
     isNavOpen() {
       const inputOpen = document.getElementById('hamburger').checked;
       const backDrop = document.querySelector('#home-page, #about, #detail');
+      const menuLink = document.querySelector('header a');
 
       if (inputOpen === true) {
         backDrop.classList.add('blurin');
+        document.querySelector('.drawer-list').style.transform = 'translate(0, 0)';
+        document.querySelector('body').style.overflow = 'hidden';
+        menuLink.classList.sadd('menu_transition');
       } else {
         backDrop.classList.remove('blurin');
+        document.querySelector('.drawer-list').style.transform = 'translate(0, -100%)';
+        document.querySelector('body').style.overflow = 'scroll';
+        menuLink.classList.remove('menu_transition');
       }
     },
     isPageScrolled() {
